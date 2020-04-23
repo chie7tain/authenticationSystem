94b1c7fedb35e62cb7247390563f68f6588ea4aa
Ifeanyi Okwuobi <fredrickokwuobi@gmail.com>
2020-04-23 22:19:20 +0100

    add styles

--- forgot_password.php
+++ forgot_password.php
@@ -1,3 +1,31 @@
 <?php include_once('lib/header.php')?>
-	reset your password here
+	
+	<h3>Forgot password</h3>
+	<p>Provide the email address associated with your accout</p>
+
+	<div>
+		<form method="POST" action="processForgot.php">
+			<?php
+				if(isset($_SESSION["error"]) && !empty($_SESSION["error"])){
+
+					echo "<span style='color:red'>" .$_SESSION["error"]."</span>";
+					// removes session variables
+					// session_unset();
+					// destroys session
+					session_destroy();
+				}
+			?>
+			<div>
+				<label>Email:</label><br>
+				<input
+				<?php 
+					if(isset($_SESSION['email'])){
+						echo "value=" . $_SESSION['email'];
+					}
+				?>
+				 type="email" name="email" placeholder="email@example.com">
+			</div>
+			<input type="submit" name="" value="send reset code">
+		</form>
+	</div>
 <?php include_once('lib/footer.php')?>
---
+++ processforgot.php
@@ -0,0 +1,45 @@
+<?php session_start();
+
+$errorCount = 0;
+
+$email = $_POST["email"] !=="" ? $_POST["email"]:$errorCount++;
+
+$_SESSION["email"] = $email;
+
+if($errorCount >0){
+	$session_error = "You have " . $errorCount . "error";
+	if($errorCount > 1){
+		$session_error .= "s";
+	}
+	$session_error.= " in your form submission ";
+	$_SESSION["error"] = $session_error;
+	header("Location: forgot_password.php");
+}else{
+	$allUsers = scandir("db/users/");
+	$countAllUsers = count($allUsers);
+
+	// check if user exists 
+	for($counter = 0; $counter <= $countAllUsers; $counter++ ){
+		$currentUser = $allUsers[$counter];
+		if($currentUser == $email . ".json"){
+			$subject = "Password Reset Link";
+			$message = "A password reset has been initiated from your account, if you did not initiate this reset, please disregard this message, otherwise, visit: localhost/snh/reset.php";
+			$headers = "From: no-reply@snh.org" . "\r\n" . 
+			"CC: seyi@snh.org";
+			
+			$try = mail($email,$subject,$message,$headers);
+			print_r($try);
+			die();
+			if($try){
+				$_SESSION['error'] = "Password reset has been sent to your email: " . $email;
+				header("Location: login.php");
+			}else{
+				$_SESSION['error'] = "Something went wrong, we could not send password reset to: " . $email;
+				header("Location: forgot_password.php");
+			}
+			die();
+		}
+	}
+	$_SESSION['error'] = "This Email: " . $email. " is not registered ERR: ";
+	header("Location: forgot_password.php");
+}
